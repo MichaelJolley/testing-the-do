be1498b844da380e11040f4aa02b608c2a276b9f
Michael Jolley <mike@sparcapp.io>
2020-11-03 15:55:17 -0600

    Doing the do

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const connectionString = 'postgresql://postgres:postgres@db:5432/postgres';
+const pool = new Pool({
+  connectionString
+});
+
+const name = "a sdf asdf asf";
+const username = new Date().getTime().toString();
+const passwordHash = "asdfa sdf asdfa";
+
+pool
+  .query('INSERT INTO users(name, username, password) VALUES($1, $2, $3) RETURNING name, username', [name, username, passwordHash])
+  .then((res) => {
+
+    pool.query('SELECT name, username from users')
+      .then(res => {
+        for (let i = 0; i < res.rowCount; i++) {
+          console.dir(res.rows[i]);
+        }
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
