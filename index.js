e0b1b409677a7ed8f8d9fa94c3248916f29dbb5b
Michael Jolley <mike@sparcapp.io>
2020-11-04 20:02:26 +0000

    The heck...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgresql://postgres:postgres@db:5432/postgres';
+const connectionString = 'postgresql://postgres:postgres@localhost:5432/postgres';
 const pool = new Pool({
   connectionString
 });
